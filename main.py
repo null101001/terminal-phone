071296b3cf75ccf3b38e1bfe6d42048e28355e65
mikhail <mikhail.waddell@gmail.com>
2024-06-07 08:07:40 -0400

    add main

---
+++ main.py
@@ -0,0 +1,10 @@
+import pyfiglet
+
+
+def main_phone():
+    # print(f"{'*'*15} This is a phone {'*'*15}")
+    result = pyfiglet.figlet_format("This  is  a  Phone !")
+    print(result)
+
+
+main_phone()
